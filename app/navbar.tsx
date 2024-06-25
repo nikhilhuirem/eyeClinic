7d07b6b8b84e68e3b08c75f7e855b2a2ae0608d5
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-06-25 10:34:38 +0530

    fixed the navbar section

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,22 +1,31 @@
-'use client'
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+    const pathname = usePathname();
+
     return (
         <div className="bg-gray-800 text-white p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-semibold">Hospital Management System</h1>
                 <nav>
                     <ul className="flex space-x-4">
-                        <li>
-                            <Link href="/" className="hover:underline">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="/reception" className="hover:underline">Reception</Link>
-                        </li>
-                        <li>
-                            <Link href="/doctors" className="hover:underline">Doctors</Link>
-                        </li>
+                        {pathname !== '/' && (
+                            <li>
+                                <Link href="/" className="hover:underline">Home</Link>
+                            </li>
+                        )}
+                        {pathname !== '/reception' && pathname !== '/doctors' && (
+                            <li>
+                                <Link href="/reception" className="hover:underline">Reception</Link>
+                            </li>
+                        )}
+                        {pathname !== '/doctors' && pathname !== '/reception' && (
+                            <li>
+                                <Link href="/doctors" className="hover:underline">Doctors</Link>
+                            </li>
+                        )}
                     </ul>
                 </nav>
             </div>
